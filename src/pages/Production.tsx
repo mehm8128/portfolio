3eb920e37d8dfdc9da156603d54cc2d6cfe702d2
mehm8128 <mehm8128@gmail.com>
2022-01-10 01:10:04 +0900

    制作物ページをいい感じにした

--- src/pages/Production.tsx
+++ src/pages/Production.tsx
@@ -1,7 +1,23 @@
 import styles from './Production.module.css'
 
 function Production() {
-  return <div className={styles.production}></div>
+  const productionList = [{name: '', image: '', description: ''}]
+  const listItems = productionList.map((item, index) => (
+    <li key={index} className={styles.li}>
+      <h1>{item.name}</h1>
+      <img src={item.image} alt={item.name} />
+      <div>{item.description}</div>
+    </li>
+  ))
+
+  return (
+    <div className={styles.production}>
+      <div className={styles.header}>
+        <span className={styles.title}>制作物</span>
+      </div>
+      <ul className={styles.ul}>{listItems}</ul>
+    </div>
+  )
 }
 
 export default Production
