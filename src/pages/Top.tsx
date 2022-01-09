564e0bad1b38f04080136e3f2883f0731b67865b
mehm8128 <mehm8128@gmail.com>
2022-01-10 00:13:36 +0900

    トップ画面にボタン設置

--- src/pages/Top.tsx
+++ src/pages/Top.tsx
@@ -1,11 +1,28 @@
 import styles from './Top.module.css'
+import icon from '../assets/mehm8128.png'
+import {useNavigate} from 'react-router-dom'
 
 function Top() {
+  const navigate = useNavigate()
   return (
-    <div>
-      mehm8128
+    <div className={styles.top}>
+      <div className={styles.header}>
+        <img src={icon} alt="icon" className={styles.iconImage} />
+        <span className={styles.name}>mehm8128</span>
+      </div>
       <br />
-      ちょっとした自己紹介
+      <div className={styles.text}>ちょっとした自己紹介</div>
+      <div className={styles.buttons}>
+        <button onClick={() => navigate('/introduction')} className={styles.button}>
+          自己紹介
+        </button>
+        <button onClick={() => navigate('/production')} className={styles.button}>
+          制作物
+        </button>
+        <button onClick={() => navigate('/links')} className={styles.button}>
+          リンク
+        </button>
+      </div>
     </div>
   )
 }
