de96142040630364eeb2ae6af6bae6a208815cf1
mehm8128 <mehm8128@gmail.com>
2022-01-10 00:16:48 +0900

    自己紹介ページ

--- src/pages/Introduction.tsx
+++ src/pages/Introduction.tsx
@@ -1,7 +1,15 @@
 import styles from './Introduction.module.css'
 
 function Introuction() {
-  return <div className={styles.introduction}></div>
+  return (
+    <div className={styles.introduction}>
+      <div className={styles.header}>
+        <span className={styles.name}>自己紹介</span>
+      </div>
+      <br />
+      <div className={styles.text}>しっかりした自己紹介</div>
+    </div>
+  )
 }
 
 export default Introuction
