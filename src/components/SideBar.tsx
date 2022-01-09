eb6621d87aa9a010a9ba4fb4a34b33aa89259a5d
mehm8128 <mehm8128@gmail.com>
2022-01-09 23:18:05 +0900

    サイドバーのCSS

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -5,18 +5,12 @@ function SideBar() {
   const navigate = useNavigate()
   return (
     <div className={styles.sideBar}>
-      <button onClick={() => navigate('/')} className={styles.button}>
+      <button onClick={() => navigate('/')} className={styles.button1}>
         ホーム
       </button>
-      <button onClick={() => navigate('/')} className={styles.button}>
-        自己紹介
-      </button>
-      <button onClick={() => navigate('/')} className={styles.button}>
-        制作物
-      </button>
-      <button onClick={() => navigate('/')} className={styles.button}>
-        リンク
-      </button>
+      <button onClick={() => navigate('/')}>自己紹介</button>
+      <button onClick={() => navigate('/')}>制作物</button>
+      <button onClick={() => navigate('/')}>リンク</button>
     </div>
   )
 }
