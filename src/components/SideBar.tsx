f450e460e211e83ad0a96794a0e2f7beccd3738a
mehm8128 <mehm8128@gmail.com>
2022-01-10 23:28:01 +0900

    色変更とホバー時の動作追加

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,23 +1,34 @@
-import {useNavigate} from 'react-router-dom'
+import {useLocation, useNavigate} from 'react-router-dom'
 import styles from './SideBar.module.css'
 
 function SideBar() {
   const navigate = useNavigate()
+  const location = useLocation()
   return (
     <div className={styles.sideBar}>
-      <button onClick={() => navigate('/')} className={styles.button}>
+      <button
+        onClick={() => navigate('/')}
+        className={location.pathname === '/' ? styles.buttonHere : styles.buttonNotHere}>
         ホーム
       </button>
-      <button onClick={() => navigate('/introduction')} className={styles.button}>
+      <button
+        onClick={() => navigate('/introduction')}
+        className={location.pathname === '/introduction' ? styles.buttonHere : styles.buttonNotHere}>
         自己紹介
       </button>
-      <button onClick={() => navigate('/productions')} className={styles.button}>
+      <button
+        onClick={() => navigate('/productions')}
+        className={location.pathname === '/productions' ? styles.buttonHere : styles.buttonNotHere}>
         制作物
       </button>
-      <button onClick={() => navigate('/links')} className={styles.button}>
+      <button
+        onClick={() => navigate('/links')}
+        className={location.pathname === '/links' ? styles.buttonHere : styles.buttonNotHere}>
         リンク
       </button>
-      <button onClick={() => navigate('/contact')} className={styles.button}>
+      <button
+        onClick={() => navigate('/contact')}
+        className={location.pathname === '/contact' ? styles.buttonHere : styles.buttonNotHere}>
         お問い合わせ
       </button>
     </div>
