64c7048dc51a78e04d29a463741fbd9377a0cf9c
mehm8128 <mehm8128@gmail.com>
2022-01-10 15:27:13 +0900

    お問い合わせ機能ほぼ完成

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,16 +1,45 @@
 import {useState} from 'react'
 import styles from './Contact.module.css'
+import {init} from 'emailjs-com'
+import emailjs from 'emailjs-com'
 
 function Contact() {
   const [name, setName] = useState('')
   const [mail, setMail] = useState('')
   const [message, setMessage] = useState('')
+
+  function onClickSendMail(e: React.MouseEvent<HTMLButtonElement>) {
+    e.preventDefault()
+    if (name !== '' && message !== '') {
+      init(process.env.REACT_APP_USER_ID!)
+
+      const emailjsServiceId = process.env.REACT_APP_EMAILJS_SERVICE_ID
+      const emailjsTemplateId = process.env.REACT_APP_EMAILJS_TEMPLATE_ID
+
+      const templateParams = {
+        name: name,
+        mail: mail,
+        message: message,
+      }
+
+      emailjs.send(emailjsServiceId!, emailjsTemplateId!, templateParams).then(() => {
+        setName('')
+        setMail('')
+        setMessage('')
+        alert('送信されました')
+      })
+    } else {
+      alert('名前と本文は必須です')
+    }
+  }
   return (
     <div className={styles.contact}>
       <div className={styles.header}>
         <span className={styles.title}>お問い合わせ</span>
       </div>
-      <div>説明</div>
+      <div className={styles.text}>
+        何かありましたらご連絡ください。名前と本文のみ必須ですが、メールアドレスが記入されていない場合返信できません。
+      </div>
       <div className={styles.form}>
         <form>
           <input
@@ -33,7 +62,9 @@ function Contact() {
             onChange={(e) => setMessage(e.target.value)}
             className={styles.input2}
           />
-          <input type="submit" value="送信" className={styles.submitButton} />
+          <button className={styles.submitButton} onClick={(e) => onClickSendMail(e)}>
+            送信
+          </button>
         </form>
       </div>
     </div>
