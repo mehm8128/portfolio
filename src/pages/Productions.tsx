4937205ddc98d55caafb1b035b10f3b19a5e7e44
mehm8128 <mehm8128@gmail.com>
2022-01-10 23:29:12 +0900

    map使って一覧作るとリンクを貼れないので大規模変更

--- src/pages/Productions.tsx
+++ src/pages/Productions.tsx
@@ -4,37 +4,65 @@ function Production() {
   const productionList = [
     {
       name: 'Palamo',
-      description:
-        'traPの2021冬ハッカソン(ピクシブ協賛)で作った場所法援助アプリ。クライアントを担当した。クライアントはReact+TypeScript、サーバーはgolangで作られている。',
       link: 'https://frontend-opal-delta-19.vercel.app/',
     },
     {
       name: 'SNS for engineers',
-      description: 'Qiitaのこの記事を見てクライアントを作った。Reactで作った。',
       link: 'https://sns-for-engineers.vercel.app/',
     },
     {
       name: 'クリスマスツリー飾りつけアプリ',
-      description: 'クリスマス前に作ったクリスマスツリー飾りつけWebアプリ。Reactで作った。',
       link: 'https://event-app-ecru.vercel.app/',
     },
   ]
-  const listItems = productionList.map((item, index) => (
-    <li key={index} className={styles.li}>
-      <h1>{item.name}</h1>
-      <div className={styles.itemContent}>
-        <div className={styles.description}>{item.description}</div>
-        <a href={item.link}>{item.link}</a>
-      </div>
-    </li>
-  ))
 
   return (
     <div className={styles.production}>
       <div className={styles.header}>
         <span className={styles.title}>制作物</span>
       </div>
-      <ul className={styles.ul}>{listItems}</ul>
+      <ul className={styles.ul}>
+        <li className={styles.li}>
+          <h1>
+            <a href={productionList[0].link} className={styles.link}>
+              {productionList[0].name}
+            </a>
+          </h1>
+          <div className={styles.itemContent}>
+            <div className={styles.description}>
+              traPの2021冬ハッカソン(ピクシブ協賛)で作った場所法援助アプリ。クライアントを担当した。クライアントはReact+TypeScript、サーバーはgolangで作られている。
+            </div>
+          </div>
+        </li>
+        <li className={styles.li}>
+          <h1>
+            <a href={productionList[1].link} className={styles.link}>
+              {productionList[1].name}
+            </a>
+          </h1>
+          <div className={styles.itemContent}>
+            <div className={styles.description}>
+              Qiitaの
+              <a href="https://qiita.com/HawkClaws/items/599d7666f55e79ef7f56" className={styles.link}>
+                この記事
+              </a>
+              を見てクライアントを作った。Reactで作った。
+            </div>
+          </div>
+        </li>
+        <li className={styles.li}>
+          <h1>
+            <a href={productionList[2].link} className={styles.link}>
+              {productionList[2].name}
+            </a>
+          </h1>
+          <div className={styles.itemContent}>
+            <div className={styles.description}>
+              クリスマス前に作ったクリスマスツリー飾りつけWebアプリ。Reactで作った。
+            </div>
+          </div>
+        </li>
+      </ul>
     </div>
   )
 }
