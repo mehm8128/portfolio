968e6710949d2c6245fcf531d536a56b00c2f41b
mehm8128 <mehm8128@gmail.com>
2022-01-10 16:00:03 +0900

    自己紹介の文章書いた

--- src/pages/Introduction.tsx
+++ src/pages/Introduction.tsx
@@ -7,7 +7,22 @@ function Introuction() {
         <span className={styles.title}>自己紹介</span>
       </div>
       <br />
-      <div className={styles.text}>しっかりした自己紹介</div>
+      <div className={styles.text}>
+        2021年
+        <br />
+        4月 東京工業大学工学院に入学
+        <br />
+        5月 東京工業大学デジタル創作同好会traPに入部、本格的にAtcoderを始める
+        <br />
+        6月 traPの部内ハッカソンで部内で制作された音楽をまとめて一覧化するWebアプリ「Qloud」制作のフロントエンドを担当
+        <br />
+        10月 Atcoderで茶色になる
+        <br />
+        12月 traPの部内ハッカソン（ピクシブ協賛）で場所法援助Webアプリ「Palamo」制作のフロントエンドを担当
+        <br />
+        2022年
+        <br />
+      </div>
     </div>
   )
 }
--- src/pages/Top.tsx
+++ src/pages/Top.tsx
@@ -9,7 +9,24 @@ function Top() {
         <span className={styles.name}>mehm8128</span>
       </div>
       <br />
-      <div className={styles.text}>ちょっとした自己紹介</div>
+      <div className={styles.text}>
+        ここはmehm8128のポートフォリオサイトです
+        <br />
+        左のサイドバーから詳細な自己紹介や制作物、リンクなどのページへ飛ぶことができます
+        <br />
+        <br />
+        所属：東京工業大学工学院情報通信系、東京工業大学デジタル創作同好会traP
+        <br />
+        やってること：Web（主にフロントエンド）、競プロ（Atcoder）、CTF
+        <br />
+        <br />
+        Web：フロントエンドはReact、Vue、TypeScriptなど、バックエンドはGolang
+        <br />
+        競プロ：主にPython。C++は勉強中
+        <br />
+        CTF：
+        <br />
+      </div>
     </div>
   )
 }
