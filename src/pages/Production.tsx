9cc094557a9aa5a8aa6d431bfba8d864bdd9b908
mehm8128 <mehm8128@gmail.com>
2022-01-10 13:14:19 +0900

    制作物ページ詳細を書いた

--- src/pages/Production.tsx
+++ src/pages/Production.tsx
@@ -1,12 +1,31 @@
 import styles from './Production.module.css'
 
 function Production() {
-  const productionList = [{name: '', image: '', description: ''}]
+  const productionList = [
+    {
+      name: 'Palamo',
+      description:
+        'traPの2021冬ハッカソン(ピクシブ協賛)で作った場所法援助アプリ。クライアントを担当した。クライアントはReact+TypeScript、サーバーはgolangで作られている。',
+      link: 'https://frontend-opal-delta-19.vercel.app/',
+    },
+    {
+      name: 'SNS for engineers',
+      description: 'Qiitaのこの記事を見てクライアントを作った。Reactで作った。',
+      link: 'https://sns-for-engineers.vercel.app/',
+    },
+    {
+      name: 'クリスマスツリー飾りつけアプリ',
+      description: 'クリスマス前に作ったクリスマスツリー飾りつけWebアプリ。Reactで作った。',
+      link: 'https://event-app-ecru.vercel.app/',
+    },
+  ]
   const listItems = productionList.map((item, index) => (
     <li key={index} className={styles.li}>
       <h1>{item.name}</h1>
-      <img src={item.image} alt={item.name} />
-      <div>{item.description}</div>
+      <div className={styles.itemContent}>
+        <div className={styles.description}>{item.description}</div>
+        <a href={item.link}>{item.link}</a>
+      </div>
     </li>
   ))
 
