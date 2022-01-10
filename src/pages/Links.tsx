cbf3f7e15228fc97671a76a7c1bd1752218ba0d5
mehm8128 <mehm8128@gmail.com>
2022-01-10 23:28:19 +0900

    CSS微修正

--- src/pages/Links.tsx
+++ src/pages/Links.tsx
@@ -13,14 +13,18 @@ function Links() {
   const listItems1 = linkList.slice(0, Math.ceil(linkList.length / 2)).map((item, index) => (
     <li key={index} className={styles.li}>
       <span className={styles.itemContent}>
-        <a href={item.link}>{item.name}</a>
+        <a href={item.link} className={styles.link}>
+          {item.name}
+        </a>
       </span>
     </li>
   ))
   const listItems2 = linkList.slice(Math.ceil(linkList.length / 2)).map((item, index) => (
     <li key={index} className={styles.li}>
       <span className={styles.itemContent}>
-        <a href={item.link}>{item.name}</a>
+        <a href={item.link} className={styles.link}>
+          {item.name}
+        </a>
       </span>
     </li>
   ))
