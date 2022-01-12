f1bf2e04d03a63161c1832a313858854a2788364
mehm8128 <mehm8128@gmail.com>
2022-01-12 14:52:52 +0900

    自己紹介修正

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -15,12 +15,12 @@ function Home() {
         左のサイドバーから詳細な自己紹介や制作物、リンクなどのページへ飛ぶことができます。
         <br />
         <br />
-        所属：東京工業大学工学院情報通信系、東京工業大学デジタル創作同好会traP
+        所属：東京工業大学工学院、東京工業大学デジタル創作同好会traP（SysAd班、アルゴリズム班）
         <br />
         やってること：Web（主にフロントエンド）、競プロ（Atcoder）、CTF
         <br />
         <br />
-        Web：フロントエンドはReact、Vue、TypeScriptなど、バックエンドはGolang。
+        Web：フロントエンドはReact、Vue、TypeScriptなど、バックエンドはGolang（勉強中）。
         <br />
         競プロ：主にPython。C++は勉強中。
         <br />
