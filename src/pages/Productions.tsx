c14cf53e669b87a6b9f0c5836fcb50df5e71120c
mehm8128 <mehm8128@gmail.com>
2022-01-14 20:55:44 +0900

    制作物追加

--- src/pages/Productions.tsx
+++ src/pages/Productions.tsx
@@ -3,16 +3,21 @@ import styles from './Productions.module.css'
 function Production() {
   const productionList = [
     {
-      name: 'Palamo',
-      link: 'https://frontend-opal-delta-19.vercel.app/',
-    },
-    {
       name: 'SNS for engineers',
       link: 'https://sns-for-engineers.vercel.app/',
     },
     {
+      name: 'Palamo',
+      link: 'https://frontend-opal-delta-19.vercel.app/',
+    },
+
+    {
       name: 'クリスマスツリー飾りつけアプリ',
       link: 'https://event-app-ecru.vercel.app/',
+    },
+    {
+      name: '簡易SNS',
+      link: 'https://concise-sns.vercel.app/',
     },
   ]
 
@@ -30,7 +35,11 @@ function Production() {
           </h1>
           <div className={styles.itemContent}>
             <div className={styles.description}>
-              traPの2021冬ハッカソン(ピクシブ協賛)で作った場所法援助アプリ。クライアントを担当した。クライアントはReact+TypeScript、サーバーはgolangで作られている。
+              Qiitaの
+              <a href="https://qiita.com/HawkClaws/items/599d7666f55e79ef7f56" className={styles.link}>
+                この記事
+              </a>
+              を見てクライアントを作った。Reactで作った。
             </div>
           </div>
         </li>
@@ -42,14 +51,11 @@ function Production() {
           </h1>
           <div className={styles.itemContent}>
             <div className={styles.description}>
-              Qiitaの
-              <a href="https://qiita.com/HawkClaws/items/599d7666f55e79ef7f56" className={styles.link}>
-                この記事
-              </a>
-              を見てクライアントを作った。Reactで作った。
+              traPの2021冬ハッカソン(ピクシブ協賛)で作った場所法援助アプリ。クライアントを担当した。クライアントはReact+TypeScript、サーバーはgolangで作られている。
             </div>
           </div>
         </li>
+
         <li className={styles.li}>
           <h1>
             <a href={productionList[2].link} className={styles.link}>
@@ -62,9 +68,34 @@ function Production() {
             </div>
           </div>
         </li>
+        <li className={styles.li}>
+          <h1>
+            <a href={productionList[3].link} className={styles.link}>
+              {productionList[3].name}
+            </a>
+          </h1>
+          <div className={styles.itemContent}>
+            <div className={styles.description}>
+              golangで書いたサーバーをherokuでデプロイし、Reactで適当にクライアントを作って名前と本文を投稿できるようにした。RDBMSにはPostgreSQLを使っている。
+            </div>
+          </div>
+        </li>
       </ul>
     </div>
   )
 }
-
+/*
+<li className={styles.li}>
+  <h1>
+    <a href={productionList[数字].link} className={styles.link}>
+      {productionList[数字].name}
+    </a>
+  </h1>
+  <div className={styles.itemContent}>
+    <div className={styles.description}>
+      説明
+    </div>
+  </div>
+</li>
+*/
 export default Production
